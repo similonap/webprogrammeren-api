04492f1983b1ce86bf60e24c41fada3edec8fd2f
Similon Andie (p118722) <andie.similon@ap.be>
2021-12-02 15:57:24 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+const dinosaurs = require('./dinosaurs.json');
+const express = require('express')
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+app.get('/dinosaurs', (req, res) => {
+  res.json(dinosaurs);
+})
+
+app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
