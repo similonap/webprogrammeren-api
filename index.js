2cd2135e0b730afeb88cae39bd140d23bc6896dc
Similon Andie (p118722) <andie.similon@ap.be>
2021-12-02 16:11:12 +0100

    Add secure endpoint

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const dinosaurs = require('./dinosaurs.json');
 const express = require('express')
 const app = express()
 
+app.get('/secure/dinosaurs', (req,res) => {
+  let apiKey = undefined;
+  if (req.header('Authorization')) {
+    apiKey = req.header('Authorization');
+  } else {
+    apiKey = req.query.API_KEY;
+  }
+
+  if (apiKey === "ef2a5495fa4c80") {
+    res.json(dinosaurs);    
+  } else {
+    res.json({error: "Invalid API key"}, 401);
+  }
+});
+
 app.get('/dinosaurs', (req, res) => {
   res.json(dinosaurs);
-})
+});
 
 app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
